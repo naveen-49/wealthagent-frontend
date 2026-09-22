dbc327f8ae7e0b42db6bdf018751a4db568efdca
naveen-49 <naveenyadavmettela@gmail.com>
2026-09-22 12:07:08 +0530

    Connect frontend to production backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://nexgile-wealthagentstats-3.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -39,3 +39,4 @@ api.interceptors.response.use(
 );
 
 export default api;
+
